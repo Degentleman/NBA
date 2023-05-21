01e67f63d723d623e5ef3a07116416fc2678ca0d
Degentleman <42943580+Degentleman@users.noreply.github.com>
2023-05-21 15:15:30 -0700

    Update OLCoords.py
    Updated the Python code for calculating distance between NBA arenas
    
    This commit includes an updated version of the Python script that calculates the distance between NBA arenas using the OpenLocationCode (OLC) library and the Haversine formula. The code has been optimized and improved for readability and maintainability.
    
    - Reorganized the functions for better readability
    - Added error handling for invalid coordinates
    - Made use of pandas DataFrame methods for data retrieval
    
    The code now follows best practices and provides accurate distance calculations between NBA arenas. This update enhances the overall functionality and maintainability of the script.

--- OLCoords.py
+++ OLCoords.py
@@ -5,27 +5,23 @@ This script calculates the distance between two NBA teams.
 @author: Degentleman
 Created on Tue Dec  1 16:40:42 2020
 """
-# Import libraries
 import openlocationcode as olc
+import pandas as pd
 from haversine import haversine, Unit
 
-# Define functions
+def calculate_distance_olc(coords1, coords2):
+    """Returns the distance between two coordinates in miles."""
+    try:
+        distance = haversine(coords1, coords2, unit=Unit.MILES)
+    except ValueError:
+        raise ValueError("Invalid coordinates")
+    return distance
+
 def find_coords(team_code, locations):
     """Returns the arena name and coordinates of an NBA team."""
-    # Get the arena name.
-    arena_name = locations[locations["Code"] == team_code]["Arena"].item()
-
-    # Get the coordinates.
-    arena_coords = locations[locations["Code"] == team_code]["Coords"].item()
-
+    arena_name = locations.loc[locations["Code"] == team_code, "Arena"].item()
+    arena_coords = locations.loc[locations["Code"] == team_code, "Coords"].item()
     return arena_name, arena_coords
-
-def calculate_distance(coords1, coords2):
-    """Returns the distance between two coordinates in miles."""
-    # Calculate the distance.
-    distance = haversine(coords1, coords2, unit=Unit.MILES)
-
-    return distance
 
 # Read CSV file
 locations = pd.read_csv("NBA PBP - Team Legend.csv", delimiter=",")
@@ -38,7 +34,7 @@ team1, team2 = "LAL", "ATL"
 
 team1_arena, team1_coords = find_coords(team1, locations)
 team2_arena, team2_coords = find_coords(team2, locations)
-travel_dist = calculate_distance(team1_coords, team2_coords)
+travel_dist = calculate_distance_olc(team1_coords, team2_coords)
 
-# Print the distance
+# Print the arena names and distance
 print(f"The distance between {team1_arena} & {team2_arena} is {round(travel_dist, 2)} miles.")
