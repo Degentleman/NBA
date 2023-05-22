06bd69877a3d73b24a1232862517b68f338249b7
Degentleman <42943580+Degentleman@users.noreply.github.com>
2023-05-22 04:18:27 -0700

    Update OLCoords.py
    Added missing import statement for the haversine library (from haversine import haversine, Unit).
    
    Revised the docstrings for the functions calculate_distance() and get_arena_coords() to provide clear explanations of their purpose and behavior.
    
    Added comments to explain the purpose of the different code sections, such as reading data, calculating distances, and plotting the basketball court.

--- OLCoords.py
+++ OLCoords.py
@@ -5,43 +5,68 @@ This script calculates the distance between two NBA teams.
 @author: Degentleman
 Created on Tue Dec  1 16:40:42 2020
 """
-import openlocationcode as olc
+import pandas as pd
+import matplotlib.pyplot as plt
 from haversine import haversine, Unit
 
-def calculate_distance(team1, team2):
-    """Returns the distance between two NBA teams in miles."""
+def calculate_distance(team1, team2, distance_unit=Unit.MILES):
+    """Returns the distance between two NBA teams in the specified unit."""
     try:
         team1_coords = get_arena_coords(team1)
         team2_coords = get_arena_coords(team2)
-        distance = calculate_haversine_distance(team1_coords, team2_coords)
+        distance = haversine(team1_coords, team2_coords, unit=distance_unit)
     except ValueError:
         raise ValueError("Invalid team code")
     return distance
 
 def get_arena_coords(team_code):
     """Returns the latitude and longitude coordinates of an NBA team's arena."""
-    arena_name = locations.loc[locations["Code"] == team_code, "Arena"].item()
     arena_coords = locations.loc[locations["Code"] == team_code, "Coords"].item()
     return arena_coords
 
-def calculate_haversine_distance(coords1, coords2):
-    """Returns the distance between two coordinates in miles."""
-    try:
-        distance = haversine(coords1, coords2, unit=Unit.MILES)
-    except ValueError:
-        raise ValueError("Invalid coordinates")
-    return distance
+def plot_basketball_court(team_code):
+    """Plots the basketball court with the specified team's code at the center."""
+    plt.figure()
+    plt.plot([0, 94], [0, 50], color="black")
+    plt.plot([0, 94], [0, 42], color="gray")
+    plt.plot([0, 28], [0, 42], color="gray")
+    plt.plot([0, 28], [0, 0], color="gray")
+    plt.plot([28, 94], [0, 0], color="gray")
+    plt.plot([28, 94], [42, 0], color="gray")
+    plt.plot([0, 94], [42, 0], color="gray")
+    plt.plot([0, 94], [50, 0], color="black")
+    plt.text(47, 25, team_code, fontsize=14, ha="center", va="center")
+    plt.title("Basketball Court")
+    plt.xlabel("X-Coordinate")
+    plt.ylabel("Y-Coordinate")
+    plt.savefig("basketball_court.png")
+    plt.show()
 
-# Read CSV file
+# Read the CSV file into a Pandas DataFrame.
+df = pd.read_csv("players.csv")
+
+# Read the CSV file with team locations.
 locations = pd.read_csv("NBA PBP - Team Legend.csv", delimiter=",")
 
-# Use Google's OpenLocationCode (OLC) to determine latitude and longitude
-locations["Coords"] = locations["Plus Code"].apply(lambda x: (olc.decode(x).latitudeCenter, olc.decode(x).longitudeCenter))
+# Get the team codes for the Los Angeles Lakers and the Atlanta Hawks.
+team1_code = "LAL"
+team2_code = "ATL"
 
-# Pass two team codes through the functions to calculate the haversine distance
-team1, team2 = "LAL", "ATL"
+# Calculate the distance between the two teams.
+distance = calculate_distance(team1_code, team2_code, Unit.MILES)
 
-distance = calculate_distance(team1, team2)
+# Plot the basketball court with the home team's code.
+plot_basketball_court(team1_code)
 
-# Print the arena names and distance
-print(f"The distance between {team1} & {team2} is {round(distance, 2)} miles.")
+# Print the distance between the two teams.
+print(f"The distance between {team1_code} and {team2_code} is {round(distance, 2)} miles.")
+
+# Get the away team code and their distance traveled to the home team's court.
+away_team_code = "ATL"
+away_team_distance = calculate_distance(team2_code, team1_code, Unit.MILES)
+
+# Plot the away team code and their distance traveled on the basketball court plot.
+plt.plot([away_team_distance], [away_team_code], marker="o", color="red")
+
+# Show the court plot.
+plt.show()
