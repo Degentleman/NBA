0230476cc488c3589523ecee1be8b4657dd0bb02
Degentleman <42943580+Degentleman@users.noreply.github.com>
2023-05-22 17:33:18 -0700

    Update OLCoords.py
    OpenLocationCode changed to Plus

--- OLCoords.py
+++ OLCoords.py
@@ -5,69 +5,39 @@ This script calculates the distance between two NBA teams.
 @author: Degentleman
 Created on Tue Dec  1 16:40:42 2020
 """
+
+import plus as olc
 import pandas as pd
-import matplotlib.pyplot as plt
-from haversine import haversine, Unit
+import haversine as hs
+from haversine import Unit
 
+# Search function returns NBA team's arena name and the coordinates tuple.
+def find_coords(team_code, locations):
+    arena_name = locations[(locations.Code == team_code)].Arena.item()
+    arena_coords = locations[(locations.Code == team_code)].Coords.item()
+    return(arena_name, arena_coords)
 
-def calculate_distance(team1, team2, distance_unit=Unit.MILES):
-    """Returns the distance between two NBA teams in the specified unit."""
-    try:
-        team1_coords = get_arena_coords(team1)
-        team2_coords = get_arena_coords(team2)
-        distance = haversine(team1_coords, team2_coords, unit=distance_unit)
-    except ValueError:
-        raise ValueError("Invalid team code")
-    return distance
+# Function calculates distance (in miles) between two coordinate tuples.
+def calc_dist(arena1, arena2):
+    dist = hs.haversine(arena1, arena2, unit=Unit.MILES)    
+    return(dist)
 
+# CSV of NBA arenas and their corresponding Google Plus Codes.
+locations = pd.read_csv('NBA PBP - Team Legend.csv', delimiter=",")
 
-def get_arena_coords(team_code):
-    """Returns the latitude and longitude coordinates of an NBA team's arena."""
-    arena_coords = locations.loc[locations["Code"] == team_code, "Coords"].item()
-    return arena_coords
+# Use Google's OpenLocationCode (OLC) to determine Latitude and Longitude.
+locations['Coords'] = locations['Plus Code'].apply(lambda x: (olc.decode(x).latitudeCenter, olc.decode(x).longitudeCenter))
 
+# Get the team codes from the user.
+home_team_code = input("Enter the code for the home team: ")
+away_team_code = input("Enter the code for the away team: ")
 
-def plot_basketball_court(team_code):
-    """Plots the basketball court with the specified team's code at the center."""
-    plt.figure()
-    plt.plot([0, 94], [0, 50], color="black")
-    plt.plot([0, 94], [0, 42], color="gray")
-    plt.plot([0, 28], [0, 42], color="gray")
-    plt.plot([0, 28], [0, 0], color="gray")
-    plt.plot([28, 94], [0, 0], color="gray")
-    plt.plot([28, 94], [42, 0], color="gray")
-    plt.plot([0, 94], [42, 0], color="gray")
-    plt.plot([0, 94], [50, 0], color="black")
-    plt.text(47, 25, team_code, fontsize=14, ha="center", va="center")
-    plt.title("Basketball Court")
-    plt.xlabel("X-Coordinate")
-    plt.ylabel("Y-Coordinate")
-    plt.savefig("basketball_court.png")
-    plt.show()
-
-
-# Read the CSV file with team locations.
-locations = pd.read_csv("NBA PBP - Team Legend.csv", delimiter=",")
-
-# Get the team codes for the Los Angeles Lakers and the Atlanta Hawks.
-team1_code = "LAL"
-team2_code = "ATL"
+# Find the arena names and coordinates for the two teams.
+home_team_arena, home_team_coords = find_coords(home_team_code, locations)
+away_team_arena, away_team_coords = find_coords(away_team_code, locations)
 
 # Calculate the distance between the two teams.
-distance = calculate_distance(team1_code, team2_code, Unit.MILES)
-
-# Plot the basketball court with the home team's code.
-plot_basketball_court(team1_code)
+travel_dist = calc_dist(home_team_coords, away_team_coords)
 
 # Print the distance between the two teams.
-print(f"The distance between {team1_code} and {team2_code} is {round(distance, 2)} miles.")
-
-# Get the away team code and their distance traveled to the home team's court.
-away_team_code = "ATL"
-away_team_distance = calculate_distance(team2_code, team1_code, Unit.MILES)
-
-# Plot the away team code and their distance traveled on the basketball court plot.
-plt.plot([away_team_distance], [away_team_code], marker="o", color="red")
-
-# Show the court plot.
-plt.show()
+print('The distance between '+home_team_arena+' & '+away_team_arena+' is '+str(round(travel_dist,2))+' miles.')
