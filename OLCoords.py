ef8d548a42501a2ed02e0d093866be3f58a476a9
Degentleman <42943580+Degentleman@users.noreply.github.com>
2023-05-22 18:01:50 -0700

    Update OLCoords.py

--- OLCoords.py
+++ OLCoords.py
@@ -6,14 +6,42 @@ This script calculates the distance between two NBA teams.
 Created on Tue Dec  1 16:40:42 2020
 """
 
-import openlocationcode as olc
 import pandas as pd
+import openlocationcode as olc
+from haversine import haversine, Unit
 
-# CSV of NBA arenas and their corresponding Google Plus Codes.
-locations = pd.read_csv('NBA PBP - Team Legend.csv', delimiter=",")
+def calculate_distance(team1, team2):
+    """Returns the distance between two NBA teams' arenas in miles."""
+    try:
+        team1_coords = get_arena_coords(team1)
+        team2_coords = get_arena_coords(team2)
+        distance = calculate_haversine_distance(team1_coords, team2_coords)
+    except (ValueError, KeyError):
+        raise ValueError("Invalid team code")
+    return distance
 
-# Use Google's OpenLocationCode (OLC) to determine Latitude and Longitude.
-locations['Coords'] = locations['Plus Code'].apply(lambda x: (olc.decode(x).latitudeCenter, olc.decode(x).longitudeCenter))
+def get_arena_coords(team_code):
+    """Returns the latitude and longitude coordinates of an NBA team's arena."""
+    arena_coords = locations.loc[locations["Code"] == team_code, "Coords"].item()
+    return arena_coords
 
-# Print the latitude and longitude coordinates of the first row in the DataFrame.
-print(locations.iloc[0]['Coords'])
+def calculate_haversine_distance(coords1, coords2):
+    """Returns the distance between two coordinates in miles."""
+    try:
+        distance = haversine(coords1, coords2, unit=Unit.MILES)
+    except ValueError:
+        raise ValueError("Invalid coordinates")
+    return distance
+
+# Read the NBA teams' arena data from the CSV file.
+locations = pd.read_csv("NBA PBP - Team Legend.csv", delimiter=",")
+
+# Use Google's OpenLocationCode (OLC) to determine the latitude and longitude of the arenas.
+locations["Coords"] = locations["Plus Code"].apply(lambda x: (olc.decode(x).latitudeCenter, olc.decode(x).longitudeCenter))
+
+# Pass two team codes through the function to calculate the haversine distance.
+team1, team2 = "LAL", "ATL"
+distance = calculate_distance(team1, team2)
+
+# Print the arena names and distance.
+print(f"The distance between {team1} & {team2} is {round(distance, 2)} miles.")
