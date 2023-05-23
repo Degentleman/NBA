09ff4cdd8c56b9371ae5ad7b60473f84b4cfb010
Degentleman <42943580+Degentleman@users.noreply.github.com>
2023-05-23 11:48:14 -0700

    Update OLCoords.py
    Updated OLCoords to fix issues decoding Plus Codes due to improper handling of Plus Code objects. Input is required for home team and away team.

--- OLCoords.py
+++ OLCoords.py
@@ -5,43 +5,43 @@ This script calculates the distance between two NBA teams.
 @author: Degentleman
 Created on Tue Dec  1 16:40:42 2020
 """
-
-import pandas as pd
 import openlocationcode as olc
 from haversine import haversine, Unit
+import pandas as pd
 
-def calculate_distance(team1, team2):
-    """Returns the distance between two NBA teams' arenas in miles."""
-    try:
-        team1_coords = get_arena_coords(team1)
-        team2_coords = get_arena_coords(team2)
-        distance = calculate_haversine_distance(team1_coords, team2_coords)
-    except (ValueError, KeyError):
-        raise ValueError("Invalid team code")
-    return distance
+# Read the NBA PBP - Team Legend.csv file.
+locations = pd.read_csv('NBA PBP - Team Legend.csv', delimiter=",")
 
-def get_arena_coords(team_code):
-    """Returns the latitude and longitude coordinates of an NBA team's arena."""
-    arena_coords = locations.loc[locations["Code"] == team_code, "Coords"].item()
-    return arena_coords
+# Use Google's OpenLocationCode (OLC) to determine Latitude and Longitude.
+locations['Coords'] = locations['Plus Code'].apply(lambda x: (olc.decode(x).latitudeCenter, olc.decode(x).longitudeCenter))
 
-def calculate_haversine_distance(coords1, coords2):
-    """Returns the distance between two coordinates in miles."""
-    try:
-        distance = haversine(coords1, coords2, unit=Unit.MILES)
-    except ValueError:
-        raise ValueError("Invalid coordinates")
-    return distance
+# Define the function to calculate the distance between two teams.
+def calculate_distance_between_teams(team1, team2):
+  """Calculates the distance between two NBA teams.
 
-# Read the NBA teams' arena data from the CSV file.
-locations = pd.read_csv("NBA PBP - Team Legend.csv", delimiter=",")
+  Args:
+    team1: The code of the home team.
+    team2: The code of the away team.
 
-# Use Google's OpenLocationCode (OLC) to determine the latitude and longitude of the arenas.
-locations["Coords"] = locations["Plus Code"].apply(lambda x: (olc.decode(x).latitudeCenter, olc.decode(x).longitudeCenter))
+  Returns:
+    The distance between the two teams in miles.
+  """
 
-# Pass two team codes through the function to calculate the haversine distance.
-team1, team2 = "LAL", "ATL"
-distance = calculate_distance(team1, team2)
+  # Get the coordinates of the two teams.
+  home_coords = locations[locations['Code'] == team1]['Coords'].values[0]
+  away_coords = locations[locations['Code'] == team2]['Coords'].values[0]
 
-# Print the arena names and distance.
-print(f"The distance between {team1} & {team2} is {round(distance, 2)} miles.")
+  # Calculate the distance between the two teams.
+  distance = haversine(home_coords, away_coords, unit=Unit.MILES)
+
+  return distance
+
+# Prompt the user to input the home and away team's code.
+home_team_code = input("Enter the home team's code: ")
+away_team_code = input("Enter the away team's code: ")
+
+# Calculate the distance between the two teams.
+distance = calculate_distance_between_teams(home_team_code, away_team_code)
+
+# Print the distance to the console.
+print("The distance between the two teams ("+away_team_code+" @ "+home_team_code+") is {} miles.".format(round(distance,2)))
